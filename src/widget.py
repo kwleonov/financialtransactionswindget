8657b1c1e0cc962a2d4c5425284ee6c5dd4b171c
Konstantin Leonov <leonovkw@yandex.ru>
2024-11-03 11:01:37 +0300

    add module widget

---
+++ src/widget.py
@@ -0,0 +1,17 @@
+# Module widget
+
+
+def mask_account_card(s: str) -> str:
+    """Get string of type and number card or bank account and return it with mask number
+    Example:
+        Visa Platinum 7000792289606361 -> Visa Platinum 7000 79** **** 6361
+        Счет 73654108430135874305      -> Счет **4305"""
+
+    pass
+
+
+def get_date(date_time: str) -> str:
+    """Get date and time in format '2024-03-11T02:26:18.671407'
+    and return date in format 'DD.MM.YYYY' ('11.03.2004')"""
+
+    pass
