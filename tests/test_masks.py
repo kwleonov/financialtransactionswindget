b38ec04af870fa66696fc9199ce6c65b709f3779
Konstantin Leonov <leonovkw@yandex.ru>
2024-11-17 15:39:31 +0300

    refactored the test_masks module.

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,13 +1,17 @@
+import pytest
+
 from src.masks import get_mask_card_number
 
 
-def test_get_mask_card_number(card_numbers: dict[str, list[str]]) -> None:
+@pytest.mark.parametrize("card_number, mask", [
+    ("1596837868705199", "1596 83** **** 5199"),
+    ("7158300734726758", "7158 30** **** 6758"),
+    ("6831982476737658", "6831 98** **** 7658"),
+    ("8990922113665229", "8990 92** **** 5229"),
+    ("5999414228426353", "5999 41** **** 6353"),
+])
+def test_get_mask_card_number(card_number: str, mask: str) -> None:
     """testing the correctness of masking a card number."""
 
-    for i in range(len(card_numbers["card_number"])):
-        card_number = card_numbers["card_number"][i]
-        mask = card_numbers["mask"][i]
-
-        out = get_mask_card_number(card_number)
-
-        assert out != mask
+    out = get_mask_card_number(card_number)
+    assert out == mask
