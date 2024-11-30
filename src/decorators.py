b7f49970b7ec0ce56ea3c9cbdd4f191002fd4ab1
Konstantin Leonov <leonovkw@yandex.ru>
2024-11-30 10:21:33 +0300

    added a test for the log decorator when an error is called as a result of executing the wrapped function.

--- src/decorators.py
+++ src/decorators.py
@@ -5,17 +5,18 @@ def log(filename=""):
         def wrapper(*args, **kwargs):
             end_line = "\n"
             inner_name = inner.__name__
+            log_result = ""
             try:
                 result = inner(*args, **kwargs)
                 log_result = "{0} {1}".format(inner_name, "ok")
+                return result
             except Exception as e:
-                log_result = "{0} {1}: {2} Inputs: {3} {4}".format(inner_name, "err", e, *args, **kwargs)
+                log_result = "{0} {1}: {2}. Inputs: {3} {4}.".format(inner_name, "error", e, args, kwargs)
             finally:
                 if filename == "":
                     print(log_result)
                 else:
                     with open(filename, "a") as logfile:
                         logfile.write(log_result + end_line)
-            return result
         return wrapper
     return logger
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -4,7 +4,7 @@ from src.decorators import log
 
 
 def test_log(capsys):
-    """test"""
+    """test log myfunc returned string."""
 
     @log()
     def myfunc(s):
@@ -15,3 +15,18 @@ def test_log(capsys):
     result = "myfunc ok"
     answer = captured.out.rstrip()
     assert answer == result
+
+
+def test_log_error(capsys):
+    """test log myfunc with exception error."""
+
+    @log()
+    def myfunc(a, b):
+        return a + b
+
+    myfunc(1, b="2")
+    captured = capsys.readouterr()
+    result = "myfunc error: unsupported operand type(s) for +: 'int' and 'str'. Inputs: (1,) {'b': '2'}."
+    answer = captured.out.rstrip()
+
+    assert result == answer
