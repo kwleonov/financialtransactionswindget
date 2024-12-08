7de588b3bba5f9a8dda741cee943359d9e701c02
Konstantin Leonov <leonovkw@yandex.ru>
2024-12-08 15:13:18 +0300

    added the get_transaction_amount function in the utils module.

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,22 @@ def load_operations_json(filename: str) -> list[dict[str, any]]:
             pass
 
     return transaction_data
+
+
+def get_transaction_amount(transaction_data: dict[str, any]) -> float:
+    """accepts a transaction as input and returns the transaction amount
+    in rubles, data type — float. If the transaction was in USD or EUR, an external API is
+    accessed to obtain the current exchange rate and convert the transaction amount into
+    rubles. To convert currency, use the convert_currency function in
+    the external_api module."""
+
+    amount = transaction_data["operationAmount"]["amount"]
+    currency = transaction_data["operationAmount"]["currency"]["code"]
+
+    amount_float = 0.0
+    if currency != "RUB":
+        amount = convert_currency(amount, code)
+    else:
+        amount = float(amount)
+
+    return amount_float
