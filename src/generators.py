6782f25e0675e55e4d75b4b6dd4bde83fbad96e4
Konstantin Leonov <leonovkw@yandex.ru>
2024-12-14 10:39:49 +0300

    added typing hints in the generators module.

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,11 @@
 # the generators module
 
+from collections.abc import Generator, Iterable
 
-def filter_by_currency(transactions, currency_code):
+from src.utils import TransactionData
+
+
+def filter_by_currency(transactions: list[TransactionData], currency_code: str) -> Iterable[TransactionData]:
     """The filter_by_currency function, which get a list of dictionaries representing transactions as input.
     The function should return an iterator that alternately issues transactions where
     the transaction currency corresponds to the specified one (for example, USD)."""
@@ -10,7 +14,7 @@ def filter_by_currency(transactions, currency_code):
     return transactions_by_code
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list[TransactionData]) -> Generator[str]:
     """The generator transaction_descriptions, which takes a list of dictionaries with transactions and
     returns a description of each operation in turn."""
 
@@ -18,7 +22,7 @@ def transaction_descriptions(transactions):
         yield transaction["description"]
 
 
-def card_number_generator(start_number, stop_number):
+def card_number_generator(start_number: int, stop_number: int) -> Generator[str]:
     """The generator, which issues bank card numbers in the format
     XXXX XXXX XXXX XXXX, where X is the digit of the card number.
     The generator can generate card numbers in the specified range from 0000 0000 0000 0001 to 9999 9999 9999 9999.
